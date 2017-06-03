2e0a7e214e7a2370f13b71e9383c2aca61679f93
bennypaulino <santiagopro71@gmail.com>
2017-06-03 19:50:59 -0400

    Access the details of the note

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 class Note extends React.Component {
   render () {
+    const { note } = this.props;
+    
     return (
       <div className="note-container">
         Note Component
