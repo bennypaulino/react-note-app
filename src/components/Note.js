0b7a7f48e5a2654c0574cf1c2e2be57cc5f38304
bennypaulino <santiagopro71@gmail.com>
2017-06-10 18:26:30 -0400

    Adjust src/components/Note.js
    * Create new function that's triggered upon form submission.
    * Prevent default behavior to refresh page after entering new tag.

--- src/components/Note.js
+++ src/components/Note.js
@@ -11,6 +11,11 @@ class Note extends React.Component {
     this.props.submitNote(formData, this.props.note.id);
   }
 
+  onTagSubmit(e) {
+    e.preventDefault();
+    console.log("Here's a new tag!");
+  }
+
   renderTagForm() {
     if (!this.props.newTag) {
       return (
@@ -24,7 +29,7 @@ class Note extends React.Component {
       );
     } else {
       return (
-        <form>
+        <form onSubmit={(e) => this.onTagSubmit(e)}>
           <input className="tag-input"
                  type="text"
                  placeholder="Tag Name..."
