0d80868ee8f06e047c2c67980761362858e72de3
bennypaulino <santiagopro71@gmail.com>
2017-06-16 21:39:11 -0400

    Display error messages briefly

--- src/components/Flash.js
+++ src/components/Flash.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class Flash extends React.Component {
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.resetError();
+    }, 2500);
+  }
+
   render () {
     const { error } = this.props;
 
